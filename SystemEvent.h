20eab367dcc40d561bfdb91d233f6446fef5f4c3
arashelv <arashel.vargas51@login.cuny.edu>
2026-04-30 14:20:59 -0400

    SystemEvent.h

--- SystemEvent.h
+++ SystemEvent.h
@@ -1,2 +1,38 @@
 //Arashel - System event header
 // systemEvent class header
+
+
+
+#include <iostream>
+#include <string> // being used for userID, eventID etc
+#include <sstream>
+#ifndef SYSTEMEVENT_H
+#define SYSTEMEVENT_H // these are used for .h files
+// class name 1 = SystemEvent
+
+using namespace std;
+
+class SystemEvent {
+    //SystemEvent is also the base class part of inheritance. Which is why we are 
+    // using protected class since the other classes need access to the data below. 
+    // meaning "inheritance" - takes from it
+    
+    protected: // using protected because we are using inheritance
+    string userID; // Example: EID-101. Labels
+    string eventID; // Example: UID-105. Labels
+    string eventType; // login, saving, loading
+    string timestamp; // when it happened
+    string severity; // severity level - high moderate
+    
+    public: //names
+    SystemEvent (string uid, string eid, string event, string time, string sever);
+    ing into inginto
+    virtual void displayInfo();
+    virtual string toFileString();
+    virtual ~SystemEvent();
+    
+    
+}; // always for classes
+#endif   // needs to be used when using ifndef/define
+
+
