dd1ccd9fee2d9fefe865a801d0df906786657a89
arashelv <arashel.vargas51@login.cuny.edu>
2026-04-30 14:21:35 -0400

    SystemEvent.cpp

--- SystemEvent.cpp
+++ SystemEvent.cpp
@@ -1,1 +1,38 @@
 //Arashel - System event.cpp file
+
+
+#include <iostream>
+#include <sstream> 
+#include "SystemEvent.h"
+// this will have the function definitions of base class meaning
+// the functions that are being used
+// learned that :: is needed when it comes to implementation because
+// we need to know which class the function belongs to. ex: classname::
+SystemEvent::SystemEvent(string uid, string eid, string event, string time, string sever) {
+        userID = uid;
+        eventID = eid;
+        eventType = event;
+        timestamp = time;
+        severity = sever;
+}
+// needs display function(polymorphism) and file string for csv. .cpp does not use virtual
+// polymorphism helps with choosing which class to choose from
+
+
+// this will display all data that was stored through polymorphism
+    void SystemEvent::displayInfo() {
+        cout << "User ID: " << userID << endl;
+        cout << "Event ID: " << eventID << endl;
+        cout << "Event Type: " << eventType << endl;
+        cout << "Time of event: " << timestamp << endl;
+        cout << "Severity: High|Moderate|Low: " << severity << endl;
+    }
+    
+    
+    string SystemEvent::toFileString() {
+        stringstream ss;
+        ss << eventID << "," << userID << "," << eventType << ","
+        << severity << "," << timestamp;
+        return ss.str();
+    }
+    SystemEvent::~SystemEvent() {}
