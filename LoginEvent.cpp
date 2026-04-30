99495993d57e63dfd0d4c1b1d59cecc467aa7cbf
arashelv <arashel.vargas51@login.cuny.edu>
2026-04-30 14:23:17 -0400

    LoginEvent.cpp

--- LoginEvent.cpp
+++ LoginEvent.cpp
@@ -1,1 +1,28 @@
 //Arashel - LoginEvent.cpp file
+
+
+#include <iostream>
+#include <sstream>
+#include "LoginEvent.h"
+
+using namespace std;
+
+        LoginEvent::LoginEvent(string uid, string eid, string time, string sever, string lstatus)
+        : SystemEvent(uid, eid, "Login", time, sever)
+        {
+            loginStatus = lstatus;  
+        }
+        
+        void LoginEvent::displayInfo() {
+        SystemEvent::displayInfo(); // this is from SystemEvent - the base class
+        cout << "Login Status: " << loginStatus << endl;
+    }
+    
+    string LoginEvent::toFileString() {
+        stringstream ss;
+        ss << SystemEvent::toFileString() << "," << loginStatus;
+        return ss.str();
+    }
+
+    
+    
