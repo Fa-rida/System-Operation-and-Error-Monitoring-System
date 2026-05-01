cdcb3ba0718540e157ec85041b56c41ef9065c65
arashelv <arashel.vargas51@login.cuny.edu>
2026-04-30 23:03:49 -0400

    FileEvent.h

--- FileEvent.h
+++ FileEvent.h
@@ -1,1 +1,29 @@
 //Arashel - FileEvent header file
+
+
+#include <iostream>
+#include <string>
+#include "SystemEvent.h"
+#ifndef FILEEVENT_H
+#define FILEEVENT_H // needed for the .h files. all caps.
+
+using namespace std;
+
+class FileEvent : public SystemEvent { // : inheritance = related
+    private: 
+    string fileName;
+    string operationType; 
+    
+    public:
+    FileEvent(string uid, string eid, string time, string sever, string fileName, string operationType);
+
+
+    void displayInfo() override;
+    string toFileString() override;
+    
+    string getFileName(); // getter functions because they come from private and protected data members
+    string getOperationType();
+    
+};
+#endif
+    
