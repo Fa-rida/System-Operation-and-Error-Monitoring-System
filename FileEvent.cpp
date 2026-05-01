982d46a84f87d8e04939d98e4bb4fe75437af125
arashelv <arashel.vargas51@login.cuny.edu>
2026-04-30 23:06:02 -0400

    FileEvent.cpp

--- FileEvent.cpp
+++ FileEvent.cpp
@@ -1,1 +1,46 @@
 //Arashel - FileEvent.cpp file
+
+
+#include <iostream>
+#include <sstream> // because we are using stringstream
+#include <string>
+#include "FileEvent.h"
+
+
+using namespace std;
+
+// :: wthis is used for which class the data belongs to
+   FileEvent::FileEvent(string uid, string eid, string time, string sever, 
+      string fName, string opType) : SystemEvent(uid, eid, "File", time, sever)
+{
+    // the base class variables + new variables for FileEvent
+    
+    fileName = fName; 
+    operationType = opType;
+    
+}
+
+ 
+   void FileEvent::displayInfo() { // same idea as the other two classes
+       SystemEvent::displayInfo();
+       cout << "File Name: " << fileName << endl;
+       cout << "Operation Type: " << operationType << endl;
+   }
+   
+   string FileEvent::toFileString() {
+       stringstream ss;
+       ss << SystemEvent::toFileString() << "," << fileName << "," << operationType;
+       return ss.str();
+       
+   }
+   
+   // these will be the getter functions (Members)
+   string FileEvent::getFileName() { 
+       return fileName;
+
+   }
+   string FileEvent::getOperationType() {
+       return operationType;
+   }
+
+  
