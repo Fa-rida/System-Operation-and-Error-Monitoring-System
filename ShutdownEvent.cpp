ebe29e36f816c84082753508426b09a1eaf717de
arashelv <arashel.vargas51@login.cuny.edu>
2026-05-01 19:25:00 -0400

    ShutdownEvent.cpp

--- ShutdownEvent.cpp
+++ ShutdownEvent.cpp
@@ -1,1 +1,35 @@
 //Arashel - ShutdownEvent.cpp file
+
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "ShutdownEvent.h"
+
+using namespace std;
+
+// uses :: again to match the other classes + data comes from other classes
+    ShutdownEvent::ShutdownEvent(string uid, string eid, string time, string sever, 
+    string status) : SystemEvent(uid, eid, "Shutdown", time, sever) {
+     
+     // defining the variable
+     shutdownStatus = status;
+ }
+ 
+ void ShutdownEvent::displayInfo() {
+     SystemEvent::displayInfo();
+    cout << "Shutdown Status : " << shutdownStatus << endl;
+ }
+ 
+ 
+ string ShutdownEvent::toFileString() {
+     stringstream ss; 
+     ss << SystemEvent::toFileString() << ",System Shutdown," << shutdownStatus; 
+     return ss.str();
+ }
+ 
+ 
+ // getter function 
+ string ShutdownEvent::getShutdownStatus() {
+     return shutdownStatus;
+ }
