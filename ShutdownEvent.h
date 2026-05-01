b09312a4d75b7e3bd22dac9ef56c7ea4ed3c0cfd
arashelv <arashel.vargas51@login.cuny.edu>
2026-05-01 19:23:45 -0400

    ShutdownEvent.h

--- ShutdownEvent.h
+++ ShutdownEvent.h
@@ -1,1 +1,35 @@
 //Arashel - ShutdownEvent header file
+
+
+#include <iostream>
+#include <string>
+#include "SystemEvent.h"
+#ifndef SHUTDOWNEVENT_H //for the h file
+#define SHUTDOWNEVENT_H
+
+using namespace std;
+
+
+// this is inheritance from the SystemEvent
+  class ShutdownEvent : public SystemEvent { 
+    
+    private: // because its events from the system should be private
+    string shutdownStatus;
+    // these are the new variable + the rest are from the base class
+    
+    
+    public: 
+    ShutdownEvent(string uid, string eid, string time, string sever, string status);
+   //status
+    
+    
+    
+  void displayInfo() override; //override like the other classes
+
+  string toFileString() override;
+    // member function shutdownstatus
+  string getShutdownStatus();
+    
+};
+
+#endif
