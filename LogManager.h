ce7c0db088a7a3b29ddbb4cf0dca028dc14bec8f
josieperea <josie.a.glenn@gmail.com>
2026-05-10 14:23:26 -0400

    Update LogManager.h

--- LogManager.h
+++ LogManager.h
@@ -1,5 +1,5 @@
-//Farida - LogManager header file
-//Josefina - file handling functions ( your functions go in this class because we took out the file handling class to make it just 6 classes)
+//farida - logmanager.h file
+//josefina - file handling
 
 #ifndef LOGMANAGER_H
 #define LOGMANAGER_H
@@ -15,10 +15,9 @@ class LogManager{
         vector<SystemEvent*>events;
         
     public:
-
-        vector<SystemEvent*> getEvents();
-        
         void addEvent(SystemEvent* genEvent); //genEvent means generated event
+        
+        vector<SystemEvent*> getEvents();
         
         void viewAllEvents();
         
@@ -31,6 +30,10 @@ class LogManager{
         void filterBySeverity(string severity);
         
         void generateSummary();
+        
+        void saveToFile(string fileName);// will take all events from LogManager and write them into files
+        
+        void loadFromFile(string fileName);//will read all events data from a file recreat event odject
         
         template <typename T> //creating a function template to count events that match the value by severity or type.
         int countEvents(T value, string category){
@@ -52,27 +55,4 @@ class LogManager{
         
         ~LogManager();
 };
-#endif
-//Josefina File handler
-#ifndef FILEHANDLER_H
-#define FILEHANDLER_H
-
-#include <string> 
-using namespace std;
-
-
-class LogManager;//calling on LogManager (file already exists)
- 
-//Handles saving event data to files and loads into files 
-class FileHandler {
-    public:
-    //will take all events from LogManager and write them into files
-        void saveToFile(LogManager* lm, string fileName);//lm is pointer to the LogManager
-                                                        // and fileName is name to the file we have to save into 
-    //will read all event data from a file and recreate event objects
-        void loadFromFile(LogManager* lm, string fileName);//fileName is name of file to read from 
-        
-
-};
-
 #endif
